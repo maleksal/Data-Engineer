8e6b42e27dac729eba7461605dd51350bed70630
Malek salem <malek.salem.14@gmail.com>
2019-04-16 13:20:05 +0100

    update project files

---
+++ git_clone.py
@@ -0,0 +1,32 @@
+# imports
+import os
+import sys
+import git
+from tqdm import tqdm
+
+
+def clone_repository(path="../Desktop", repositories_list="file.txt"):
+    """
+    clone repositories from github.com
+    with a given repositories list
+
+    :param path: where to clone repositories
+    :param repositories_list: txt file contains repo urls
+    """
+    repos_list = open(
+        f"{repositories_list}", "r").read().split()
+
+    # Process Download
+    for repo in tqdm(repos_list):
+        try:
+            git.Git(path).clone(repo, branch='master')
+        except Exception as exception:
+            open(
+                    f'{os.getcwd()}/Log.txt', 'a').write(str(exception))
+    return
+
+
+clone_dir = sys.argv[1]
+list_repos = sys.argv[2]
+
+clone_repository(clone_dir, list_repos)
---
+++ process_data.py
@@ -0,0 +1,55 @@
+# imports
+import os
+import re
+import sys
+import fnmatch
+
+
+class ProcessData:
+
+    def __init__(self, repo_root):
+        """
+        :param repo_root: repository path
+        Ex: ../Desktop/repos/myrepository
+        """
+        self.repo_root = repo_root
+
+    def count_lines(self):
+        """
+        count lines of code function
+        return:{
+            'repository_url': repository url',
+             number of lines': Ex: 5000'
+             }
+        """
+
+        def walk(root, pattern='*'):
+
+            """ Generator for walking a directory tree."""
+
+            for root_, dirs, files in os.walk(root):
+                for file in files:
+                    if fnmatch.fnmatch(file, pattern):
+                        # if file extension match pattern '.py'
+                        yield os.path.join(root_, file)
+
+        # get number lines of code
+        number_of_lines = 0
+        for pyfile in walk(self.repo_root, pattern="*.py"):
+            with open(pyfile, encoding="utf8", errors='ignore') as f:
+                num = len(f.readlines())
+                number_of_lines += num
+
+        # get repository url for .git/config file
+        with open(f'{self.repo_root}/.git/config') as r:
+            url = r.read()
+            repository_url = re.findall(
+                'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', url)
+        return {'repository_url': f'{repository_url}', 'number of lines': f'{number_of_lines}'}
+
+
+# get repository path from command line
+path = sys.argv[1]
+
+process_data = ProcessData(path)
+process_data.count_lines()
